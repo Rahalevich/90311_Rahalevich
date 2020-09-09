f3bb522d227e8a83079e2876644c891fca17fa7f
Aliaksandr Rahalevich <Lostminsk@gmail.com>
2020-09-10 00:31:18 +0300

    Добавьте файлы проекта.

---
+++ 90311_Rahalevich/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _90311_Rahalevich.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
